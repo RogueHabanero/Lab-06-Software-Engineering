f50575798b8e51febd93fa8cd6aa8964ca0cc821
RogueHabanero <roguehabanero@protonmail.com>
2022-10-22 14:07:35 -0500

    Added subtraction function

--- mod_two.py
+++ mod_two.py
@@ -2,16 +2,21 @@
 #Date 10/22/2022
 #Assignment: Lab 06
 
-def modify_two(a, b):
+def add_two(a, b):
     return a + b
+
+def sub_two(a, b):
+    return a - b
 
 def main():
     a = int(input())
     b = int(input())
 
-    summation = modify_two(a, b)
-
+    summation = add_two(a, b)
     print(f"The sum of {a} and {b} is: {summation}")
+
+    subtraction = sub_two(a, b)
+    print(f"The sum of {a} and {b} is: {subtraction}")
 
 if __name__ == "__main__":
     main()
